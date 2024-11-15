c7f1b8e66869a8f82f24ae76261807ddcf05fb5f
Alejandro Cisneros <josealejandrocisner@gmail.com>
2024-11-15 15:39:55 -0600

    Add files via upload

---
+++ vaciarcarrito.php
@@ -0,0 +1,8 @@
+<?php
+//Aqui se boora el Carrito de Compras
+unlink("carritocompras.txt");
+
+echo "<CENTER><H2>El Carrito De Compras se ha Vaciado</H2>";
+echo "<BR><BR><A HREF='javascript:Window.Close();'>Cerrar</A>";
+echo "</CENTER>;"
+?>
